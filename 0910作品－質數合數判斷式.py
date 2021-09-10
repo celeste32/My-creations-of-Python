0672eab8116909ff907d422eb4b38977aa59581e
celeste32 <80192055+celeste32@users.noreply.github.com>
2021-09-10 17:14:54 +0800

    Add files via upload

---
+++ 0910作品－質數合數判斷式.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 10 17:09:09 2021
+
+@author: abc06
+"""
+
+i = int(input('輸入一個正整數：'))
+count = 0
+for n in range(2,i):
+    if i % n == 0:
+        print('此數為合數')
+        break
+        count += 1
+    elif n == i-1 :
+        print("此數為質數")
+        break
+if i == 2 :
+    print('此數為質數')
